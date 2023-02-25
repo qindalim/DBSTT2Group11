8e8645fb394dcbdbeefd711e546da73938631144
soyNstacks <at4nxy@gmail.com>
2023-02-25 14:39:15 +0800

    add navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,28 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { Link, BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import NewClaim from './componets/newClaim'
 import Login from './componets/login'
 import Home from "./componets/home"
+import NavRouter from "./navRouter"
 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Router>
-          <Routes> 
-            <Route path='/' element={<Login/>}  />
-            <Route path='/newclaim' element={<NewClaim/>} />
-            <Route path='/home' element={<Home/>} />
-          </Routes>
-        </Router>
-      </header>
+      <nav className="navbar navbar-expand navbar-dark bg-dark">
+          <li className="nav-item">
+            <a href='http://localhost:3000/'>Login</a>
+          </li>
+          <li className="nav-item">
+            <a href='http://localhost:3000/home'>Home</a>
+          </li>
+          <li className="nav-item">
+            <a href='http://localhost:3000/newclaim'>Add claim</a>
+          </li>
+      </nav>
+      <NavRouter/>
+
 		</div>
 	);
 }
