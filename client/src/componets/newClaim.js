aae7b9776f152b84253c12b0e5a1723b8c0eeebd
mabufu14 <113176785+mabufu14@users.noreply.github.com>
2023-02-25 12:00:08 +0800

    Added create claim page

--- client/src/componets/newClaim.js
+++ client/src/componets/newClaim.js
@@ -1,9 +1,97 @@
-import React from 'react'
+import * as React from 'react';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
 
-function newClaim() {
+
+function NewClaim() {
   return (
-    <div>newClaim</div>
-  )
+    <React.Fragment>
+      <Typography variant="h6" gutterBottom>
+        Submit New Claim
+      </Typography>
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            required
+            id="firstName"
+            name="firstName"
+            label="First name"
+            fullWidth
+            variant="standard"
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            required
+            id="lastName"
+            name="lastName"
+            label="Last name"
+            fullWidth
+            variant="standard"
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            required
+            id="receipNumber"
+            name="receipNumber"
+            label="Receipt Number"
+            fullWidth
+            variant="standard"
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            required  
+            id="dateValue"
+            name="dateValue"
+            label="Date"
+            fullWidth
+            variant="standard"
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            required
+            id="claimAmount"
+            name="claimAmount"
+            label="Claim Amount"
+            fullWidth
+            variant="standard"
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            required
+            id="purpose"
+            name="purpose"
+            label="Purpose of Expenditure"
+            fullWidth
+            variant="standard"
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <FormControlLabel
+            control={<Checkbox color="secondary" name="saveAddress" value="yes" />}
+            label="Follow up claim"
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            required
+            id="previousClaim"
+            name="previousClaim"
+            label="Previous Claim Number"
+            fullWidth
+            variant="standard"
+          />
+        </Grid>
+      </Grid>
+    </React.Fragment>
+  );
 }
 
-export default newClaim
+export default NewClaim;
