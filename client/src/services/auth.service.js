fb08840515386271c80e80c125d9858fe0457a09
soyNstacks <at4nxy@gmail.com>
2023-02-25 10:57:49 +0800

    set up login base template

---
+++ client/src/services/auth.service.js
@@ -0,0 +1,45 @@
+/**
+    login(): POST {username, password} & save JWT to Local Storage
+    logout(): remove JWT from Local Storage
+    register(): POST {username, email, password}
+    getCurrentUser(): get stored user information (including JWT)
+*/
+
+import axios from "axios";
+
+// dummy 
+const API_URL = "http://localhost:8080/api/auth/";
+
+class AuthService {
+    login(username, password) {
+        return axios
+            .post(API_URL + "signin", {
+                username,
+                password
+            })
+            .then(response => {
+                if (response.data.accessToken) {
+                    localStorage.setItem("user", JSON.stringify(response.data));
+                }
+                return response.data;
+            });
+    }
+
+    logout(){
+        localStorage.removeItem("user");
+    }
+
+    register(username, email, password) {
+        return axios.post(API_URL + "signup", {
+            username,
+            email,
+            password
+        });
+    }
+
+    getCurrentUser() {
+        return JSON.parse(localStorage.getItem('user'));;
+    }
+}
+
+export default new AuthService();
