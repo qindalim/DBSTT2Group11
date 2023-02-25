5b8a20601066dcad2ccc82c2c2a82a6db32dc1f7
Yong Xuan <yongxuan@Yong-Xuans-MacBook-Air.local>
2023-02-25 12:24:04 +0800

    Home Page display of claims

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import './App.css';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import NewClaim from './componets/newClaim'
@@ -12,9 +13,25 @@ function App() {
           </Routes>
         </Router>
         
+=======
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+import Home from "./componets/home";
+
+
+function App() {
+	return (
+		<div className = "App">
+      <header className = "App-header">
+        <Router> 
+          <Routes>
+            <Route path='/home' element={<Home/>} />
+          </Routes>
+        </Router>
+>>>>>>> Stashed changes
       </header>
-    </div>
-  );
+		</div>
+	);
 }
 
 export default App;
